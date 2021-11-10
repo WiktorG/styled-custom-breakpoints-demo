5e91ea3b933b93201e533ac33c187410568f353e
wgala <wiktor.gala@grupawp.pl>
2021-11-10 17:15:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled from "styled-components"
+
+import { up, down, only, between } from 'styled-breakpoints'
+import { useBreakpoint } from "styled-breakpoints/react-styled"
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+`
+
+const Heading = styled.h1`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 12px;
+  ${up(1011)} {
+    font-size: 35px;
+  }
+`
+
+export const useBreakpoints = () => {
+  const isXs = useBreakpoint(down("xs"));
+  const isSm = useBreakpoint(only("sm"));
+  const isMd = useBreakpoint(only("md"));
+  const isLg = useBreakpoint(only("lg"));
+  const isXl = useBreakpoint(up("xl"));
+  const isCustom = useBreakpoint(only(1203));
+  const isCustomBeteen = useBreakpoint(between(1000, 1200));
+
+  return {
+    isXs,
+    isSm,
+    isMd,
+    isLg,
+    isXl,
+    isCustom,
+    isCustomBeteen
+  };
+};
+
+
+function Test() {
+  const { isXs, isSm, isMd, isLg, isXl, isCustom, isCustomBeteen } = useBreakpoints();
+
+  return (
+  <ul>
+    <li>isXs: {`${isXs}`}</li>
+    <li>isSm: {`${isSm}`}</li>
+    <li>isMd: {`${isMd}`}</li>
+    <li>isLg: {`${isLg}`}</li>
+    <li>isXl: {`${isXl}`}</li>
+    <li>isCustom: {`${isCustom}`}</li>
+    <li>isBetween 1000 & 1200: {`${isCustomBeteen}`}</li>
+  </ul>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Heading>Hello from custom styled breakpoints</Heading>
+      <Test />
+    </Wrapper>
   );
 }
 
